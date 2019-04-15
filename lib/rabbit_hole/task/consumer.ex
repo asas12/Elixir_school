c24d9db8c2d5ea0d807febdb72c905509e25825d
asas12 <john13john@interia.pl>
2019-04-15 22:37:43 +0200

    Solved 2nd task.

--- lib/rabbit_hole/task/consumer.ex
+++ lib/rabbit_hole/task/consumer.ex
@@ -5,7 +5,7 @@ defmodule RabbitHole.Task.Consumer do
 
   use GenServer
   require Logger
-  alias RabbitHole.Protocol.{Connection, Channel, Queue, Basic}
+  alias RabbitHole.Protocol.{Connection, Channel, Queue, Basic, Exchange}
   alias RabbitHole.Task
 
   @type consumer_ref :: any()
@@ -16,10 +16,50 @@ defmodule RabbitHole.Task.Consumer do
 
   @spec start(Exchange.t(), Task.kind(), opts()) :: {:ok, consumer_ref()}
   def start(task_exchange, task_kind, opts \\ []) do
+    GenServer.start_link(__MODULE__, [{:exchange, task_exchange}, {:task_kind, task_kind} ]++ opts)
   end
 
   @spec stop(consumer_ref()) :: :ok
   def stop(ref) do
+    GenServer.stop(ref)
   end
+
+  # Callbacks
+
+  def init(args) do
+    # open connection
+    {:ok, connection} = Connection.open()
+    {:ok, channel} = Channel.open(connection)
+
+    # create exchange
+    :ok = Exchange.declare(channel, args[:exchange], :topic)
+
+    # set up queue
+    {:ok, queue} = Queue.declare(channel, "", [:exclusive])
+    Queue.bind(channel, queue, args[:exchange], routing_key: Task.topic(args[:task_kind]))
+    {:ok, tag} = Basic.consume(channel, queue)
+
+    # save connection
+    {:ok,
+      %{chan: channel,
+        conn: connection,
+        exchange: args[:exchange],
+        queue: args[:queue],
+        tag: tag,
+        processor: args[:processor] || &default_processor/1}}
+  end
+
+  def handle_info({:basic_deliver, message, _meta}, state) do
+    state.processor.(Task.from_message(message), self())
+    {:noreply, state}
+  end
+
+  def terminate(_reason, state) do
+    {:ok, _} = Basic.cancel(state.chan, state.tag)
+    :ok = Channel.close(state.chan)
+    :ok = Connection.close(state.conn)
+  end
+
+  defp default_processor(msg), do: IO.puts("Got message: #{inspect msg}")
 
 end
--- lib/rabbit_hole/task/producer.ex
+++ lib/rabbit_hole/task/producer.ex
@@ -17,14 +17,42 @@ defmodule RabbitHole.Task.Producer do
 
   @spec start(Exchange.t()) :: {:ok, producer_ref()}
   def start(exchange) do
+    GenServer.start_link(__MODULE__, exchange)
   end
 
   @spec stop(producer_ref()) :: :ok
   def stop(ref) do
+    GenServer.stop(ref)
   end
 
   @spec publish(producer_ref(), Task.t(), [Basic.publish_opt()]) :: :ok
   def publish(ref, task, opts \\ []) do
+    GenServer.cast(ref, {:publish, %{task: task, opts: opts}})
+  end
+
+  # Callbacks
+
+  def init(exchange) do
+    # open connection
+    {:ok, connection} = Connection.open()
+    {:ok, channel} = Channel.open(connection)
+
+    # create exchange
+    :ok = Exchange.declare(channel, exchange, :topic)
+
+    # save connection
+    {:ok, %{chan: channel, conn: connection, exchange: exchange}}
+  end
+
+  def handle_cast({:publish, data}, state) do
+    Basic.publish(state.chan, state.exchange, Task.topic(data.task), Task.to_message(data.task), data.opts)
+    {:noreply, state}
+  end
+
+
+  def terminate(_reason, state) do
+    :ok = Channel.close(state.chan)
+    :ok = Connection.close(state.conn)
   end
 
 end
